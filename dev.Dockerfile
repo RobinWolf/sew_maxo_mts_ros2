878fa860a82d25de6f8a52c9a315b2681a4b0aca
Robin Wolf <woro1021@h-ka.de>
2024-04-22 17:14:51 +0200

    fill develop workspace files

---
+++ dev.Dockerfile
@@ -0,0 +1,36 @@
+##############################################################################
+##                           1. stage: Base Image                           ##
+##############################################################################
+ARG ROS_DISTRO=humble
+FROM osrf/ros:$ROS_DISTRO-desktop as base
+
+# Configure DDS
+COPY dds_profile.xml /opt/misc/dds_profile.xml
+ENV FASTRTPS_DEFAULT_PROFILES_FILE=/opt/misc/dds_profile.xml
+
+# Create user
+ARG USER=logilab
+ARG PASSWORD=automaton
+ARG UID=1000
+ARG GID=1000
+ENV USER=$USER
+RUN groupadd -g $GID $USER \
+    && useradd -m -u $UID -g $GID -p "$(openssl passwd -1 $PASSWORD)" \
+    --shell $(which bash) $USER -G sudo
+
+# Setup workpace
+USER $USER
+RUN mkdir -p /home/$USER/ros2_ws/src
+WORKDIR /home/$USER/ros2_ws
+
+##############################################################################
+##                           2. stage: set up gazebo                        ##
+##############################################################################
+FROM base as gazebo_testenviroment
+
+#install some ros packages
+USER root
+RUN apt-get update && apt-get install -y ros-humble-xacro
+RUN apt-get update && apt-get install -y ros-humble-joint-state-publisher-gui
+RUN apt-get update && apt-get install -y ros-humble-gazebo
+USER ${USER}
---
+++ run_dev.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+##############################################################################
+##                   Build the image, using dev.Dockerfile                  ##
+##############################################################################
+ROS_DISTRO=humble
+
+uid=$(eval "id -u")
+gid=$(eval "id -g")
+
+#pass some arguments and settings to the dev.Dockerfile while building the image (dev.Dockerfile)
+#name of the image builded here: utomaton-dev/ros-render:"ROS-Distribution eg humble"
+docker build \
+  --build-arg ROS_DISTRO="$ROS_DISTRO" \
+  --build-arg UID="$uid" \
+  --build-arg GID="$gid" \
+  -f dev.Dockerfile \
+  -t automaton-dev/ros-render:"$ROS_DISTRO" .
+
+##############################################################################
+##                            Run the container                             ##
+##############################################################################
+SRC_CONTAINER=/home/logilab/ros2_ws/src
+SRC_HOST="$(pwd)"/src                           #use a src on host machine to develop code, mout it to the container to run code inside the container
+
+docker run \
+  --name gazebo_testenviroment \
+  --rm \
+  -it \
+  --net=host \
+  -v "$SRC_HOST":"$SRC_CONTAINER":rw \
+  -e DISPLAY="$DISPLAY" \
+  automaton-dev/ros-render:"$ROS_DISTRO"
+
+# display and network access is already passed to the container
