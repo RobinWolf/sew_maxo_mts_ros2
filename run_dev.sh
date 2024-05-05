3d257d14ef4333b4384453b833e31c42812ef536
Robin Wolf <woro1021@h-ka.de>
2024-05-05 12:00:33 +0200

    remove privileged to only pass js0

--- run_dev.sh
+++ run_dev.sh
@@ -31,7 +31,6 @@ docker run \
   -v "$SRC_HOST":"$SRC_CONTAINER":rw \
   -e DISPLAY="$DISPLAY" \
   --device=/dev/input/js0:/dev/input/js0 \
-  --privileged \
   logilab-gazebo-dev/ros-render:"$ROS_DISTRO"
 
 # display, joystick 0 input and network acess added to the container
