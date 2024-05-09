f9f95ae10a0ff69f6b93ab65d96629f163dbf657
Robin Wolf <woro1021@h-ka.de>
2024-05-09 18:27:54 +0200

    change yaml dir

--- src/sew_agv_navigation/launch/joystick.launch.py
+++ src/sew_agv_navigation/launch/joystick.launch.py
@@ -10,18 +10,18 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    sim_package = "gazebo_testenviroment"
+    navigation_package = "sew_agv_navigation"
 
     #declare launch arguments
     declared_arguments = []
     declared_arguments.append(
-    DeclareLaunchArgument('use_sim_time', default_value='true',
+    DeclareLaunchArgument('use_sim_time', default_value='false',
                             description='Set to "true" if you want to use the joystick with gazebo, set to "fasle" if you use real hardware.'))
     
     use_sim_time = LaunchConfiguration('use_sim_time')   
     
     #define nodes to launch
-    joy_params = os.path.join(get_package_share_directory(sim_package),'config','joystick.yaml')
+    joy_params = os.path.join(get_package_share_directory(navigation_package),'config', 'joystick', 'joystick.yaml')
     teleop_joy_node = Node(
         package='teleop_twist_joy',
         executable='teleop_node',
