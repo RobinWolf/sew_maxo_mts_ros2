37d2885592f9ea7a8f527534f67638581c0ba1bc
Robin Wolf <woro1021@h-ka.de>
2024-05-11 18:02:24 +0200

    use experimental configs

--- src/sew_agv_navigation/launch/navigation.launch.py
+++ src/sew_agv_navigation/launch/navigation.launch.py
@@ -15,9 +15,8 @@ from nav2_common.launch import RewrittenYaml
 def generate_launch_description():
 
 
-    param_file_name = 'navigation.yaml' 
+    param_file_name = 'navigation_experimental.yaml' 
     map_file_name = '240509_raw.yaml' 
-    #map_file_name = 'map.yaml' 
     navigation_package = "sew_agv_navigation"
 
     default_param_dir = os.path.join(get_package_share_directory(navigation_package), 'config', 'navigation', param_file_name)
