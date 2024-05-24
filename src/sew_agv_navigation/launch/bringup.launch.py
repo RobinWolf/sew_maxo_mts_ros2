a3e6cdc3c8093d5a2fcd5bdc5343b4db105488b8
Robin Wolf <woro1021@h-ka.de>
2024-05-24 10:41:32 +0200

    add new description package with prefixes

--- src/sew_agv_navigation/launch/bringup.launch.py
+++ src/sew_agv_navigation/launch/bringup.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "tf_prefix",
-            default_value='""',
+            default_value='sew_',
             description="Prefix for the links and joints in the robot cell",
         )
     )
