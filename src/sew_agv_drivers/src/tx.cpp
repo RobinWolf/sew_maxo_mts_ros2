45c198a46fc4709fe9e1080678b816a45c05ddf8
Robin Wolf <woro1021@h-ka.de>
2024-06-10 17:33:33 +0200

    change namespace

--- src/sew_agv_drivers/src/tx.cpp
+++ src/sew_agv_drivers/src/tx.cpp
@@ -1,25 +1,25 @@
 #include "sew_agv_drivers/tx.hpp"
 #include <cstring>
 
-namespace sew_agv_drivers
+namespace sew_agv_connection
 {
-void AgvTxMsg::set_speed(double speed)
-{
-  speed_ = speed;
-}
+  void AgvTxMsg::set_speed(double speed)
+  {
+    speed_ = speed;
+  }
 
-void AgvTxMsg::set_direction(double x, double y)
-{
-  direction_x_ = x;
-  direction_y_ = y;
-}
+  void AgvTxMsg::set_direction(double x, double y)
+  {
+    direction_x_ = x;
+    direction_y_ = y;
+  }
 
-std::vector<uint8_t> AgvTxMsg::serialize() const
-{
-  std::vector<uint8_t> data(sizeof(double) * 3);
-  std::memcpy(data.data(), &speed_, sizeof(double));
-  std::memcpy(data.data() + sizeof(double), &direction_x_, sizeof(double));
-  std::memcpy(data.data() + sizeof(double) * 2, &direction_y_, sizeof(double));
-  return data;
-}
-}  // namespace sew_agv_drivers
+  std::vector<uint8_t> AgvTxMsg::serialize() const
+  {
+    std::vector<uint8_t> data(sizeof(double) * 3);
+    std::memcpy(data.data(), &speed_, sizeof(double));
+    std::memcpy(data.data() + sizeof(double), &direction_x_, sizeof(double));
+    std::memcpy(data.data() + sizeof(double) * 2, &direction_y_, sizeof(double));
+    return data;
+  }
+}  
