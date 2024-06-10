33d9c9aac2e2224352cf6516cca2259d17e63c26
Robin Wolf <woro1021@h-ka.de>
2024-06-10 17:11:14 +0200

    first fixes in the src file (function definitions and passed types) --> now compile errors inside the methods

--- src/sew_agv_drivers/src/sew_agv_hardware_interface.cpp
+++ src/sew_agv_drivers/src/sew_agv_hardware_interface.cpp
@@ -5,78 +5,88 @@
 
 namespace sew_agv_drivers
 {
-hardware_interface::return_type AgvHardwareInterface::configure(const hardware_interface::HardwareInfo & info)
-{
-  if (configure_default(info) != hardware_interface::return_type::OK) {
-    return hardware_interface::return_type::ERROR;
+
+  AgvHardwareInterface::~AgvHardwareInterface() {
+    //Destruct Instances if controller manager is shut down via ctrl + C, because on_deactivate isnt called then
+    on_deactivate(rclcpp_lifecycle::State());
   }
 
-  // Get parameters
-  std::string agv_host = info.hardware_parameters["agv_host"];
-  int agv_port = std::stoi(info.hardware_parameters["agv_port"]);
-  std::string local_ip = info.hardware_parameters["local_ip"];
-  int local_port = std::stoi(info.hardware_parameters["local_port"]);
+  hardware_interface::CallbackReturn AgvHardwareInterface::on_init(const hardware_interface::HardwareInfo & info)
+  {
+    if (configure_default(info) != hardware_interface::return_type::OK) {
+      return hardware_interface::return_type::ERROR;
+    }
 
-  // Initialize endpoint
-  endpoint_ = std::make_shared<AgvEndpoint>(agv_host, agv_port, local_ip, local_port);
+    // Get parameters
+    std::string agv_host = info.hardware_parameters["agv_host"];
+    int agv_port = std::stoi(info.hardware_parameters["agv_port"]);
+    std::string local_ip = info.hardware_parameters["local_ip"];
+    int local_port = std::stoi(info.hardware_parameters["local_port"]);
 
-  return hardware_interface::return_type::OK;
-}
+    // Initialize endpoint
+    endpoint_ = std::make_shared<AgvEndpoint>(agv_host, agv_port, local_ip, local_port);
 
-std::vector<hardware_interface::StateInterface> AgvHardwareInterface::export_state_interfaces()
-{
-  std::vector<hardware_interface::StateInterface> state_interfaces;
-  state_interfaces.emplace_back(hardware_interface::StateInterface(
-    info_.joints[0].name, hardware_interface::HW_IF_VELOCITY, &speed_));
-  state_interfaces.emplace_back(hardware_interface::StateInterface(
-    info_.joints[0].name, "direction_x", &direction_x_));
-  state_interfaces.emplace_back(hardware_interface::StateInterface(
-    info_.joints[0].name, "direction_y", &direction_y_));
-  return state_interfaces;
-}
+    return hardware_interface::return_type::OK;
+  }
 
-std::vector<hardware_interface::CommandInterface> AgvHardwareInterface::export_command_interfaces()
-{
-  std::vector<hardware_interface::CommandInterface> command_interfaces;
-  command_interfaces.emplace_back(hardware_interface::CommandInterface(
-    info_.joints[0].name, hardware_interface::HW_IF_VELOCITY, &speed_));
-  command_interfaces.emplace_back(hardware_interface::CommandInterface(
-    info_.joints[0].name, "direction_x", &direction_x_));
-  command_interfaces.emplace_back(hardware_interface::CommandInterface(
-    info_.joints[0].name, "direction_y", &direction_y_));
-  return command_interfaces;
-}
+  std::vector<hardware_interface::StateInterface> AgvHardwareInterface::export_state_interfaces()
+  {
+    std::vector<hardware_interface::StateInterface> state_interfaces;
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      info_.joints[0].name, hardware_interface::HW_IF_VELOCITY, &speed_));
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      info_.joints[0].name, "direction_x", &direction_x_));
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      info_.joints[0].name, "direction_y", &direction_y_));
+    return state_interfaces;
+  }
 
-hardware_interface::return_type AgvHardwareInterface::start()
-{
-  endpoint_->start();
-  return hardware_interface::return_type::OK;
-}
+  std::vector<hardware_interface::CommandInterface> AgvHardwareInterface::export_command_interfaces()
+  {
+    std::vector<hardware_interface::CommandInterface> command_interfaces;
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      info_.joints[0].name, hardware_interface::HW_IF_VELOCITY, &speed_));
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      info_.joints[0].name, "direction_x", &direction_x_));
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      info_.joints[0].name, "direction_y", &direction_y_));
+    return command_interfaces;
+  }
 
-hardware_interface::return_type AgvHardwareInterface::stop()
-{
-  endpoint_->stop();
-  return hardware_interface::return_type::OK;
-}
+  hardware_interface::CallbackReturn AgvHardwareInterface::on_activate(const rclcpp_lifecycle::State& previous_state)
+  {
+    endpoint_->start();
+    return hardware_interface::return_type::OK;
+  }
 
-hardware_interface::return_type AgvHardwareInterface::read()
-{
-  // Read data from AGV
-  // Implement according to your needs
+  hardware_interface::CallbackReturn AgvHardwareInterface::on_deactivate(const rclcpp_lifecycle::State& previous_state)
+  {
+    endpoint_->stop();
+    return hardware_interface::return_type::OK;
+  }
+  {
+    endpoint_->stop();
+    return hardware_interface::return_type::OK;
+  }
 
-  return hardware_interface::return_type::OK;
-}
+  hardware_interface::return_type AgvHardwareInterface::read(const rclcpp::Time& time, const rclcpp::Duration& period)
+  {
+    // Read data from AGV
+    // Implement according to your needs
 
-hardware_interface::return_type AgvHardwareInterface::write()
-{
-  // Send commands to AGV
-  ManualJogTxMsg msg;
-  msg.set_speed(speed_);
-  msg.set_direction(direction_x_, direction_y_);
-  endpoint_->set_msg(msg);
+    return hardware_interface::return_type::OK;
+  }
 
-  return hardware_interface::return_type::OK;
-}
+  hardware_interface::return_type AgvHardwareInterface::write(const rclcpp::Time& time, const rclcpp::Duration& period)
+  {
+    // Send commands to AGV
+    ManualJogTxMsg msg;
+    msg.set_speed(speed_);
+    msg.set_direction(direction_x_, direction_y_);
+    endpoint_->set_msg(msg);
+
+    return hardware_interface::return_type::OK;
+  }
 }  // namespace sew_agv_drivers
 
 
