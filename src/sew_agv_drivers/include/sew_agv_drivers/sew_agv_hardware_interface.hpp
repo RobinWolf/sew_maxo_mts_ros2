7da62b66a275a7458f77af989aa1cb2dbafc1489
mathias31415 <mathias31415@gmx.de>
2024-06-16 12:52:50 +0200

    removed not needed? includes

--- src/sew_agv_drivers/include/sew_agv_drivers/sew_agv_hardware_interface.hpp
+++ src/sew_agv_drivers/include/sew_agv_drivers/sew_agv_hardware_interface.hpp
@@ -4,12 +4,12 @@
 #include <cstring>
 #include "rclcpp/rclcpp.hpp"
 
-#include "hardware_interface/base_interface.hpp"
+//#include "hardware_interface/base_interface.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
-#include "hardware_interface/types/hardware_interface_status_values.hpp"
+//#include "hardware_interface/types/hardware_interface_status_values.hpp"
 
 #include "sew_agv_drivers/agv_endpoint.hpp"
 #include "sew_agv_drivers/config.h"
