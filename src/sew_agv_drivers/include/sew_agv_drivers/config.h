1e611ef827c62ba021e4168d6376dade8eb00f4f
mathias31415 <mathias31415@gmx.de>
2024-06-16 12:52:24 +0200

    removed wheel infos from config.h

--- src/sew_agv_drivers/include/sew_agv_drivers/config.h
+++ src/sew_agv_drivers/include/sew_agv_drivers/config.h
@@ -6,11 +6,6 @@
 
 struct Config
 {
-  std::string left_wheel_name = "left_wheel";
-  std::string right_wheel_name = "right_wheel";
-  float wheel_separation = 0.5;
-  float wheel_radius = 0.1;
-  
   std::string agv_ip;    // The IP address or hostname of the AGV.
   int agv_port;          // The port number of the AGV.
   std::string local_ip;  // The local IP address to bind the UDP socket.
