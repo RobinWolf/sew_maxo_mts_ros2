a2562658a175dd65753f83bf6169838def481346
Robin Wolf <woro1021@h-ka.de>
2024-07-25 16:30:54 +0200

    split run in build and start with run always

---
+++ build_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##############################################################################
+##                   Build the image, using dev.Dockerfile                  ##
+##############################################################################
+ROS_DISTRO=humble
+
+uid=$(eval "id -u")
+gid=$(eval "id -g")
+
+#pass some arguments and settings to the dev.Dockerfile while building the image (dev.Dockerfile)
+
+#--no-cache
+docker build \
+  --build-arg ROS_DISTRO="$ROS_DISTRO" \
+  --build-arg UID="$uid" \
+  --build-arg GID="$gid" \
+  -f dev.Dockerfile \
+  -t logilab-sew-maxo-mts/ros-render:"$ROS_DISTRO" .
---
+++ start_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ROS_DISTRO=humble
+SRC_CONTAINER=/home/logilab/ros2_ws/src
+SRC_HOST="$(pwd)"/src                           #use a src on host machine to develop code, mout it to the container to run code inside the container
+
+docker run \
+  --name sew_navigation \
+  --restart always \
+  -it \
+  --net=host \
+  -v "$SRC_HOST":"$SRC_CONTAINER":rw \
+  -e DISPLAY="$DISPLAY" \
+  -v /dev:/dev \
+  --privileged \
+  logilab-sew-maxo-mts/ros-render:"$ROS_DISTRO"
