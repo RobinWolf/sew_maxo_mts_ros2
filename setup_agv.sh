de085dccf925e4d1a52592c3d2a8a44b1311f109
Robin Wolf <woro1021@h-ka.de>
2024-07-26 11:41:23 +0200

    new aotostart files

--- setup_agv.sh
+++ setup_agv.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo ip addr add 192.168.10.222/24 dev eth0
+
+# Set static IP configuration using NetworkManager
+nmcli connection modify 'netplan-eth0' ipv4.addresses 192.168.10.222/24
+nmcli connection modify 'netplan-eth0' ipv4.method manual
+necli connection modify 'netplan-eth0' connection.autoconnect yes
+nmcli connection modify 'netplan-eth0' connection.autoconnect-retries -1
+nmcli connection up 'netplan-eth0'
